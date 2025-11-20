e4a9d47442d953aeb926116a43ccae60a5c103d0
fakhri-git <test@fakhri.com>
2025-11-20 11:02:26 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model_name = os.getenv("LLM_MODEL")
+model_key = os.getenv("LLM_API_KEY")
+model_provider = os.getenv("LLM_PROVIDER")
+
+chat = ChatGoogleGenerativeAI(
+    model=model_name,
+    temperature=0.0,
+    google_api_key=model_key,
+)
+
+chat
+
+template_string = """Translate the text \
+that is delimited by triple backticks \
+into a style that is {style}. \
+text: ```{text}```
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template_string)
+
+service_reply = """Hey there customer, \
+the warranty does not cover \
+cleaning expenses for your kitchen \
+because it's your fault that \
+you misused your blender \
+by forgetting to put the lid on before \
+starting the blender. \
+Tough luck! See ya!
+"""
+
+service_style_pirate = """\
+a polite tone \
+that speaks in English Pirate\
+"""
+
+service_messages = prompt_template.format_messages(
+    style=service_style_pirate,
+    text=service_reply)
+
+service_response = chat.invoke(service_messages)
+
+print(service_response.content)
